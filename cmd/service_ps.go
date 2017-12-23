2e2f0e40b105533fca336b1f1ac011cbf0b19a9f
John Pignata <john@pignata.com>
2017-12-23 10:24:37 -0500

    Add `fargate service <service name> ps`

---
+++ cmd/service_ps.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/jpignata/fargate/console"
+	EC2 "github.com/jpignata/fargate/ec2"
+	ECS "github.com/jpignata/fargate/ecs"
+	"github.com/jpignata/fargate/util"
+	"github.com/spf13/cobra"
+)
+
+var servicePsCmd = &cobra.Command{
+	Use:   "ps <service name>",
+	Short: "List running tasks in a service",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		psService(args[0])
+	},
+}
+
+func init() {
+	serviceCmd.AddCommand(servicePsCmd)
+}
+
+func psService(serviceName string) {
+	var eniIds []string
+
+	ecs := ECS.New()
+	ec2 := EC2.New()
+	tasks := ecs.DescribeTasksForService(serviceName)
+
+	for _, task := range tasks {
+		eniIds = append(eniIds, task.EniId)
+	}
+
+	enis := ec2.DescribeNetworkInterfaces(eniIds)
+
+	if len(tasks) > 0 {
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 0, 8, 1, '\t', 0)
+		fmt.Fprintln(w, "IMAGE\tSTATUS\tDESIRED STATUS\tCREATED\tIP\tCPU\tMEMORY\t")
+
+		for _, t := range tasks {
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+				t.Image,
+				util.Humanize(t.LastStatus),
+				util.Humanize(t.DesiredStatus),
+				t.CreatedAt,
+				enis[t.EniId].PublicIpAddress,
+				t.Cpu,
+				t.Memory,
+			)
+		}
+
+		w.Flush()
+	} else {
+		console.Info("No tasks found")
+	}
+}
