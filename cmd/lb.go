aa22b897ddb947b793ec626d531b7e275d2f967f
John Pignata <john@pignata.com>
2017-12-28 23:35:38 -0500

    Add copy to lb command usage

--- cmd/lb.go
+++ cmd/lb.go
@@ -9,6 +9,11 @@ const defaultTargetGroupFormat = "%s-default"
 var lbCmd = &cobra.Command{
 	Use:   "lb",
 	Short: "Manage load balancers",
+	Long: `Manage load balancers
+	
+Load balancers distribute incoming traffic between the tasks within a service
+for HTTP/HTTPS and TCP applications. HTTP/HTTPS load balancers can route to
+multiple services based upon rules you specify when you create a new service.`,
 }
 
 func init() {
