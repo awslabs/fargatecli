0478483b0f86895e2bbbd3dba08ecba4a19bbd36
Kwunhok Chan <kwunhok@gmail.com>
2019-08-28 11:52:49 +0800

    Add Hong Kong region (ap-east-1)

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,22 @@ CPU (CPU Units)    Memory (MiB)
 4096               8192 through 30720 in 1GiB increments
 `)
 
-var validRegions = []string{"ap-northeast-1", "ap-northeast-2", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "us-east-1", "us-east-2", "us-west-1", "us-west-2"}
+var validRegions = []string{
+	"ap-east-1",
+	"ap-northeast-1",
+	"ap-northeast-2",
+	"ap-south-1",
+	"ap-southeast-1",
+	"ap-southeast-2",
+	"ca-central-1",
+	"eu-central-1",
+	"eu-west-1",
+	"eu-west-2",
+	"us-east-1",
+	"us-east-2",
+	"us-west-1",
+	"us-west-2",
+}
 
 var (
 	clusterName string
